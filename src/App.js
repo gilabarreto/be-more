405f5f8c5500d98ed7b945b4ad8464eb16f2650f
Victor Gila Barreto <gilabarreto@gmail.com>
2023-01-06 07:35:59 -0700

    Minor changes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       const response = await axios.post(
         'https://api.openai.com/v1/completions',
         {
-          'model': 'text-davinci-003',
+          'model': 'text-davinci-002',
           'prompt': 'Can you please provide me a random motivational quote?',
           'temperature': 1,
           'max_tokens': 1024
@@ -43,7 +43,7 @@ function App() {
 
   const delayString = async function (string) {
 
-    // await getMotivationalQuote();
+    await getMotivationalQuote();
 
     setDelayedString("");
 
