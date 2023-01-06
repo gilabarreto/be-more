bcc03ed1c65f24c59d64b60a4a8eaa81730c1656
Victor Gila Barreto <gilabarreto@gmail.com>
2023-01-05 21:45:37 -0700

    Make an axious call to chatgtp and print result

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { useState, useEffect } from 'react';
+
+import axios from 'axios';
+
 function App() {
+
+  const [quote, setQuote] = useState("");
+
+  useEffect(() => {
+    async function getMotivationalQuote() {
+      try {
+        const response = await axios.post(
+          'https://api.openai.com/v1/completions',
+          {
+            'model': 'text-davinci-003',
+            'prompt': 'Provide me a random motivational quote.',
+            'temperature': 1,
+            'max_tokens': 1024
+          },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': process.env.REACT_APP_CHATGTP_KEY
+            },
+          },
+          {
+            proxy: {
+              host: 'localhost',
+              port: 3000
+            }
+          }
+        );
+
+        setQuote(response.data.choices[0].text)
+        console.log(response.data.choices[0].text)
+
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getMotivationalQuote()
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {quote}
         </p>
         <a
           className="App-link"
@@ -23,3 +65,4 @@ function App() {
 }
 
 export default App;
+
